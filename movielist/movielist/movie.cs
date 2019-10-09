f88fca1c83d6464a0a365e34a5c1bc03ac95a1d8
skibroinc <ethanski22@icloud.com>
2019-10-09 19:25:43 -0400

    movie list

---
+++ movielist/movielist/movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace movielist
+{
+    public class movie
+    {
+        public string title;
+        public int releaseyear;
+        public movie(string title, int releaseyear)
+        {
+            //this means it goes to the public string and int to get the var
+            this.title = title;
+            this.releaseyear = releaseyear;
+
+        }
+        public void showdetails()
+        {
+            MessageBox.Show(title + " was released in " + releaseyear);
+        }
+
+    }
+}
